6c617a3658e91ed12f034853b64c105917be0d37
andresdmorales <33564095+andresdmorales@users.noreply.github.com>
2019-05-27 16:56:45 -0700

    1

--- js/comment.js
+++ js/comment.js
@@ -1,22 +1,2 @@
-$(document).ready(function() {
-  $('#contact-form').submit(function(e) {
-    var name    = document.getElementById('inputName')
-    var email   = document.getElementById('inputEmail')
-    var message = document.getElementById('inputMessage')
-
-    if (!name.value || !email.value || !message.value) {
-      alertify.error("Please check your entries");
-      return false;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '//formspree.io/luxbicorp@gmail.com',
-        data: $('#contact-form').serialize(),
-        datatype: 'json'
-      });
-      e.preventDefault();
-      $(this).get(0).reset();
-      alertify.success("Message sent");
-    }
-  });
-});
+    var contactform =  document.getElementById('contactform');
+    contactform.setAttribute('action', '//formspree.io/' + 'luxbicorp' + '@' + 'gmail' + '.' + 'com');
